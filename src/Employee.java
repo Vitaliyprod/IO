a2742757273700950289fe9f47f79de4cbcad3b8
Vitaliyprod <vitaliyprod5@gmail.com>
2021-05-30 23:44:43 +0300

    add les11

--- src/Employee.java
+++ src/Employee.java
@@ -1,11 +1,12 @@
 import java.io.Serializable;
 
+@SuppressWarnings("all")
 public class Employee implements Serializable {
     private String name;
     private int id;
-    private transient int salary;
+    private transient Integer salary;
 
-    public Employee(String name, int id, int salary) {
+    public Employee(String name, int id, Integer salary) {
         this.name = name;
         this.id = id;
         this.salary = salary;
@@ -27,11 +28,11 @@ public class Employee implements Serializable {
         this.id = id;
     }
 
-    public int getSalary() {
+    public Integer getSalary() {
         return salary;
     }
 
-    public void setSalary(int salary) {
+    public void setSalary(Integer salary) {
         this.salary = salary;
     }
 
